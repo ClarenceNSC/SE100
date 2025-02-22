d8bfc8b67ce0f76a20cb98b572ac01cf1c247a6f
ClarenceNSC <clarence_ng@outlook.com>
2025-02-22 11:47:08 +0800

    c

--- scripts.js
+++ scripts.js
@@ -2,3 +2,8 @@ const DayOneClosingPrice = "87.14";
 const DayTwoClosingPrice = "84.22";
 
 console.log(DayOneClosingPrice + DayTwoClosingPrice)
+
+const DayOneClosePx = parseFloat (DayOneClosingPrice);
+const DayTwoClosePx = parseFloat (DayTwoClosingPrice);
+
+console.log (DayOneClosePx+DayTwoClosePx)
