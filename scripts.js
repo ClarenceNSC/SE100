7599d798e6d319cec6b0c38848383f8208a11720
ClarenceNSC <clarence_ng@outlook.com>
2025-02-23 19:52:19 +0800

    c

--- scripts.js
+++ scripts.js
@@ -1,8 +1,45 @@
-const companyName = "Metaa"
+//const companyName = "Metaa"
 
-console.log(companyName.length)
+//const changeName = companyName.substring (0,4)
+//console.log(changeName)
+//console.log(changeName.toUpperCase())
 
-console.log(companyName.length-1)
+
+//const msftData = [190.15, 194.21, 191.07, 190.44, 200.15]
+//const result = ((190.15+194.21+191.07+190.44+200.15)/5)
+//console.log (result)
+
+
+
+//const msftData = [190.15, 194.21, 191.07, 190.44, 200.15]
+//console.log(msftData+ "," +205.67)
+
+
+//const disneyData = {
+	//open: "120.54",
+	//high: "122.56",
+	//low: "120.54",
+	//close: "121.09"
+//}
+
+
+//const open = parseFloat(disneyData.open);
+//const high = parseFloat(disneyData.high);
+//const low = parseFloat(disneyData.low);
+//const close = parseFloat(disneyData.close);
+
+//const result = open - high + low - close
+//console.log (result)
+//const resultTofixed = result.toFixed(2)
+//console.log (resultTofixed)
+
+
+const disneyData = {
+	open: "120.54",
+	high: "122.56",
+	low: "120.54",
+	close: "121.09",
+}
 
 
 
