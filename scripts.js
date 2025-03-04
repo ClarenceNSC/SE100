0c0d8c14cc52588d89e98d0a38463df90fa7390c
ClarenceNSC <clarence_ng@outlook.com>
2025-03-04 23:57:55 +0800

    Lesson4

--- scripts.js
+++ scripts.js
@@ -34,15 +34,73 @@
 //console.log (resultTofixed)
 
 
+//const disneyData = {
+	//open: "120.54",
+	//high: "122.56",
+	//low: "120.54",
+	//close: "121.09",
+	//volume: "1512311"
+//}
+
+//console.log (disneyData)
+
+
+//const bacData = [190.15, 194.21, 191.07, 192.44, 129000]
+
+//let	Low = 190.15,
+//	High = 194.21,
+//	Open = 191.07,
+//	Close = 192.44,
+//	Volume = 129000
+
+//if (Close > Open)
+//	if (Volume > 100000) {
+//	console.log ("Strong Bullish");
+//} else {
+//	console.log("Bullish");
+//} 
+//  else if (Close < Open) 
+//	if (Volume > 10000) {
+//		console.log ("Strong Bearish");
+	
+ //} else {
+//	console.log ("Bearish");
+ //}
+//	else {
+//		console.log("neutral");
+//	}
+  
+
 const disneyData = {
 	open: "120.54",
 	high: "122.56",
 	low: "120.54",
 	close: "121.09",
+	volume: "90000"
 }
+const openPx = parseFloat(disneyData.open);
+const closePx = parseFloat(disneyData.close);
+const volume = parseInt(disneyData.volume);
 
-disneyData.volume = "1512311"
-console.log (disneyData)
+if (closePx > openPx) 
+	if (volume > 100000) {
+		console.log ("Strong Bullish");
+	}
+		else {
+			console.log ("Bullish");
+		}
+	
+	else if (closePx < openPx) 
+			if (volume > 10000) {
+			console.log ("Strong Bearish");
+		}
+			else {
+				console.log ("Bearish");
+			}
+			
+			else {
+				console.log ("neutral")
+			}
 
 
 // Write your code below
