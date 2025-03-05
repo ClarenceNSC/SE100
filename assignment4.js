2770ba5595ed9e17c44c46c19bba4d759bde555d
ClarenceNSC <clarence_ng@outlook.com>
2025-03-06 00:31:23 +0800

    c

---
+++ assignment4.js
@@ -0,0 +1,12 @@
+const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82];
+function findMax() {
+    let amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82];
+    let maxPrice = amdPrices[0];
+
+    for (let price of amdPrices) {
+        if (amdPrices > maxPrice) {
+            maxPrice = price;
+        }
+    }
+    console.log(maxPrice)
+}
