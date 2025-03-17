f91c9beac40fa35122f15d3ce1d3fa23267c1a03
ClarenceNSC <clarence_ng@outlook.com>
2025-03-17 23:28:00 +0800

    c

--- Lesson6/root.js
+++
@@ -1,8 +0,0 @@
-const domContainer = document.getElementById('root');
-const root = ReactDOM.createRoot(domContainer);
-root.render (<App/>)
-
-const age = 20;
-const beverage = // Write your code here
-console.log(beverage); // 'Beer' if age is 18 or more, 'Juice' otherwise
-
